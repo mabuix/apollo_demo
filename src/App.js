a6df1c9580135cf2456b8a2acc086b3b5e76250f
mabui <mabuix@gmail.com>
2018-10-14 20:05:25 +0900

    react-apolloでクエリ結果出力

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+    uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
+
+const ExchangeRates = () => (
+    <Query
+        query={gql`
+      {
+        rates(currency: "USD") {
+          currency
+          rate
+        }
+      }
+    `}
+    >
+        {({ loading, error, data }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error :(</p>;
+
+            return data.rates.map(({ currency, rate }) => (
+                <div key={currency}>
+                    <p>{`${currency}: ${rate}`}</p>
+                </div>
+            ));
+        }}
+    </Query>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <ApolloProvider client={client}>
+            <div>
+                <h2>My first Apollo app 🚀</h2>
+                <ExchangeRates/>
+            </div>
+        </ApolloProvider>
     );
   }
 }
