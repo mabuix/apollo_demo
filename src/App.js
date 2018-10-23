941bf5e646a1a978d6980bdb0a59c3d2db14f5b5
mabui <mabuix@gmail.com>
2018-10-23 18:03:16 +0900

    クエリコンポーネントのサンプル作成

--- src/App.js
+++ src/App.js
@@ -2,48 +2,38 @@ import React, { Component } from 'react';
 import './App.css';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import { Query } from "react-apollo";
-import gql from "graphql-tag";
+import ExchangeRates from './components/ExchangeRates';
+import Dogs from './components/Dogs';
+import DogPhoto from './components/DogPhoto';
 
 const client = new ApolloClient({
-    uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+    //uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+    uri: "https://nx9zvp49q7.lp.gql.zone/graphql"
 });
 
-const ExchangeRates = () => (
-    <Query
-        query={gql`
-      {
-        rates(currency: "USD") {
-          currency
-          rate
-        }
-      }
-    `}
-    >
-        {({ loading, error, data }) => {
-            if (loading) return <p>Loading...</p>;
-            if (error) return <p>Error :(</p>;
-
-            return data.rates.map(({ currency, rate }) => (
-                <div key={currency}>
-                    <p>{`${currency}: ${rate}`}</p>
-                </div>
-            ));
-        }}
-    </Query>
-);
-
 class App extends Component {
-  render() {
-    return (
-        <ApolloProvider client={client}>
-            <div>
-                <h2>My first Apollo app 🚀</h2>
-                <ExchangeRates/>
-            </div>
-        </ApolloProvider>
-    );
-  }
+    state = { selectedDog: null };
+
+    onDogSelected = ({ target }) => {
+        this.setState(() => ({ selectedDog: target.value }));
+    };
+
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <div>
+                    <h2>My first Apollo app 🚀</h2>
+                    {/*
+                    <ExchangeRates/>
+                    */}
+                    {this.state.selectedDog && (
+                        <DogPhoto breed={this.state.selectedDog} />
+                    )}
+                    <Dogs onDogSelected={this.onDogSelected}/>
+                </div>
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
