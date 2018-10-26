39ddbc5b708375f228b94b7d63e687fc094d510b
mabui <mabuix@gmail.com>
2018-10-26 18:41:14 +0900

    ミューテーションコンポーネントのサンプル作成

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import { ApolloProvider } from "react-apollo";
 import ExchangeRates from './components/ExchangeRates';
 import Dogs from './components/Dogs';
 import DogPhoto from './components/DogPhoto';
+import AddTodo from './components/AddTodo';
+import Todos from './components/Todos';
 
 const client = new ApolloClient({
-    //uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
-    uri: "https://nx9zvp49q7.lp.gql.zone/graphql"
+    // uri: "https://w5xlvm3vzz.lp.gql.zone/graphql" // ExchandeRates
+    // uri: "https://nx9zvp49q7.lp.gql.zone/graphql" // Dogs
+    uri: "https://8v9r9kpn7q.lp.gql.zone/graphql" // AddTodo
 });
 
 class App extends Component {
@@ -25,11 +28,13 @@ class App extends Component {
                     <h2>My first Apollo app 🚀</h2>
                     {/*
                     <ExchangeRates/>
-                    */}
                     {this.state.selectedDog && (
                         <DogPhoto breed={this.state.selectedDog} />
                     )}
                     <Dogs onDogSelected={this.onDogSelected}/>
+                    */}
+                    <AddTodo/>
+                    <Todos/>
                 </div>
             </ApolloProvider>
         );
